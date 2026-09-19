bd86e97dc71826e52b50e691f421f59e4ab8bfde
its-zaidanwar <itszaidanwar@gmail.com>
2026-09-19 12:29:40 +0530

    react till now

---
+++ day2-jsx_comp/src/About.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = (name)=> {
+
+    console.log(name);
+    
+    return(
+        
+        <div>
+            
+            <h1>{name.name}</h1>
+        </div>
+    )
+}
+
+export default About;
---
+++ day2-jsx_comp/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import About from "./About";
+
+let App = ()=> {
+  return (
+
+  <div>
+    
+    {/* <About hello="hello" element = {<h1>i am element h1</h1>}>
+        <h1>meow</h1>
+     </About> */}
+
+     <About name = "Zaid"/>
+     </div>
+
+)
+}
+
+export default App;
